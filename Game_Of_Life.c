1a5033d1d4efd9102bf0e0e48f95b6a8c2c1e59a
Sumit Purohit <sumit.purohit@pnnl.gov>
2015-09-29 09:17:17 -0700

    version 2
    It have updated GenerateInitialGoL.
    New Simulate and DetermineState methods

--- Game_Of_Life.c
+++ Game_Of_Life.c
@@ -14,33 +14,132 @@
 #include <math.h>
 #include <stdlib.h>
 
-void GenerateInitialGoL (int rank,int p, int start,int n, int m,int matrix[n][m],int seed)
+void GenerateInitialGoL (int rank,int p, int start,int N, int effective_cols_size,int matrix[N][effective_cols_size],int seed)
 {
-int i,j;
-srand(seed);
-printf("\ncall from %d rank start row  is %d and size is %d  and number of cols\n",rank, start,n,m);
-	for (i = 0; i < n; i++)
-	     for (j = 0; j < m; j++)
-	     {
-	    	  int num;
-	    	  num = rand();
-	    	  if(num%2 == 0)
-	    		  matrix[i][j] = 1;
-	    	  else
-	    		  matrix[i][j] = 0;
-	     }
+	int i, j;
+	srand(seed);
+	printf("\ncall from %d rank start row  is %d and size is %d  and number of cols %d \n",
+			rank, start, N, effective_cols_size);
+	for (i = 0; i < N; i++)
+		for (j = 1; j < effective_cols_size-1; j++) {
+			int num;
+			num = rand();
+			if (num % 2 == 0)
+				matrix[i][j] = 1;
+			else
+				matrix[i][j] = 0;
+		}
 
+	int next_rank = (rank + 1) % p;
+	int previous_rank = (rank + p - 1) % p;
 
-	for (i = 0; i < n; i++)
-		 {
-		 for (j = 0; j < m; j++)
-			   printf("v = %d  ",matrix[i][j]);
-				 printf("\n");
-		 }
+	//send ACTUAL last col to next_rank
+	for(i=0;i<N;i++)
+	{
+		MPI_Send(&matrix[i][effective_cols_size-2], 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+	}
+	//send ACTUAL first col to previous_rank
+	for(i=0;i<N;i++)
+	{
+		MPI_Send(&matrix[i][1], 1, MPI_INT, previous_rank, 0, MPI_COMM_WORLD);
+	}
+	//update previous rank ghost column
+	for(i = 0 ; i < N; i++)
+	{
+		int a = 0;
+		MPI_Status status;
+		MPI_Recv(&a, 1, MPI_INT, previous_rank, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		matrix[i][0] = a;
+	}
 
+	//update next rank ghost column
+	for(i = 0 ; i < N; i++)
+	{
+		int a = 0;
+		MPI_Status status;
+		MPI_Recv(&a, 1, MPI_INT, next_rank, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		matrix[i][effective_cols_size-1] = a;
+	}
+
+//	printf("\n");
+//	for (i = 0; i < N; i++) {
+//		for (j = 0; j < effective_cols_size; j++)
+//			printf("v%d = %d  ",rank, matrix[i][j]);
+//		printf("\n");
+//	}
+
+}
+int getNorthState(int i,int j,int rank,int N, int effective_cols_size,int matrix[N][effective_cols_size])
+{
+	if(i==0)
+		return matrix[N][j];
+	return matrix[i-1][j];
 
 }
 
+int getSouthState(int i,int j,int rank,int N, int effective_cols_size,int matrix[N][effective_cols_size])
+{
+	if(i==N)
+		return matrix[0][j];
+	return matrix[i+1][j];
+
+}
+
+int getEastState(int i,int j,int rank,int N, int effective_cols_size,int matrix[N][effective_cols_size])
+{
+	return matrix[i][j+1];
+}
+int getWestState(int i,int j,int rank,int N, int effective_cols_size,int matrix[N][effective_cols_size])
+{
+	return matrix[i][j-1];
+}
+int DetermineState(int i,int j,int rank,int N, int effective_cols_size,int matrix[N][effective_cols_size])
+{
+	int No = getNorthState(i,j,rank,N,effective_cols_size,matrix);
+	int S = getSouthState(i,j,rank,N,effective_cols_size,matrix);
+	int E = getEastState(i,j,rank,N,effective_cols_size,matrix);
+	int W = getWestState(i,j,rank,N,effective_cols_size,matrix);
+	//int NE = getNortEastState();
+
+
+	return (No + S + E + W);
+}
+void Simulate(int g,int rank,int N, int effective_cols_size,int matrix[N][effective_cols_size])
+{
+	int tmpdata[N][effective_cols_size];
+	int i,j;
+	for (i = 0; i < N; i++)
+		for (j = 0; j < effective_cols_size; j++)
+			tmpdata[i][j] = matrix[i][j];
+
+		for (i = 0; i < N; i++) {
+			for (j = 1; j < effective_cols_size - 1; j++) {
+				int state;
+				state = DetermineState(i, j, rank, N, effective_cols_size,
+						tmpdata);
+				if ((state < 2) || (state > 3))
+					matrix[i][j] = 0;
+				else
+					matrix[i][j] = 1;
+
+			}
+		}
+
+
+	printf("\n Last State : of rank %d\n",rank);
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < effective_cols_size; j++)
+				printf("v%d-%d-%d = %d  ",rank,i,j, tmpdata[i][j]);
+			printf("\n");
+		}
+
+	printf("\n \n New State : of rank %d\n",rank);
+				for (i = 0; i < N; i++) {
+					for (j = 0; j < effective_cols_size; j++)
+						printf("v%d-%d-%d = %d  ",rank,i,j, matrix[i][j]);
+					printf("\n");
+				}
+}
 
 
 int main(int argc, char *argv[]) {
@@ -49,6 +148,7 @@ int main(int argc, char *argv[]) {
 	struct timeval t1, t2;
 	N = 16;
 	p = 2;
+	G = 1;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -71,28 +171,41 @@ int main(int argc, char *argv[]) {
 		for (i = 1; i < p; i++) {
 			MPI_Send(&randomseed[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
-		int start, end;
-			start = rank * (N / p);
-			end = (rank + 1) * (N / p) - 1;
-			int rows = (end - start);
-			int matrix[rows][N];
+		int start_index, end_index;
+		start_index = rank * (N / p);
+		end_index = (rank + 1) * (N / p) - 1;
+		int cols_size = (end_index - start_index) +1;
+		int effective_cols_size = cols_size + 2;
+		int matrix[N][effective_cols_size];
 
-			//initialize matrix part
-			GenerateInitialGoL(rank, p, start, rows, N, matrix,randomseed[0]);
+		//initialize matrix part
+		GenerateInitialGoL(rank, p, start_index, N, effective_cols_size,matrix,randomseed[0]);
+		for(i=0;i<G;i++)
+		{
+			MPI_Barrier(MPI_COMM_WORLD);
+			Simulate(i,rank,N,effective_cols_size,matrix);
+		}
 	} else {
 		int a = 0;
 		MPI_Status status;
 		MPI_Recv(&a, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
 		printf("\n ranke = %d and seend = %d", rank, a);
-		int start, end;
-			start = rank * (N / p);
-			end = (rank + 1) * (N / p) - 1;
-			int rows = (end - start);
-			int matrix[rows][N];
+		int start_index, end_index;
+		start_index = rank * (N / p);
+		end_index = (rank + 1) * (N / p) - 1;
+		int cols_size = (end_index - start_index) +1;
+		int effective_cols_size = cols_size + 2;
+		int matrix[N][effective_cols_size];
 
-			//initialize matrix part
-			GenerateInitialGoL(rank, p, start, rows, N, matrix,a);
+		//initialize matrix part
+		GenerateInitialGoL(rank, p, start_index, N, effective_cols_size,matrix,a);
+		int i;
+		for(i=0;i<G;i++)
+		{
+			MPI_Barrier(MPI_COMM_WORLD);
+			Simulate(i,rank,N,effective_cols_size,matrix);
+		}
 	}
 
 
