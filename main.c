c6ab9331e528858ac73f25868ff0e2a43ad13a39
Eric Thomas <thomasericdean@gmail.com>
2015-11-09 20:05:02 -0800

    random function is called correctly

--- main.c
+++ main.c
@@ -18,10 +18,10 @@
 
 int main(int argc, char* argv[]){
 
-
-	 MPI_Init(&argc, &argv);
-	//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	//    MPI_Comm_size(MPI_COMM_WORLD, &p);
+	int rank, p;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
 	// Get a pointer to the random number array
 	 // returned by method from previous PA.
